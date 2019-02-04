f64fc03a943b9da0f1930c88e87569db64ab59a6
Bruce Johnson <bruce@yogainternational.com>
2019-02-03 19:18:08 -0500

    updated exercise problems

--- arrays.js
+++ arrays.js
@@ -1,1 +1,11 @@
-//1. Write a function called returnArray that that takes in two arguments and when invoked returns an array containing those arguments.
+// 1. Write a function called returnArray that that takes in two arguments and when invoked
+// returns an array containing those arguments.
+
+
+// 2. Write a function called returnItemAtArrayIndex that takes in one argument (a number), searches
+// for that index on arr1, and returns the value at that index on the arr1 array.
+
+// Example
+// returnItemAtArrayIndex(1) -> would return '2'
+
+var arr1 = ['1', '2', 3, ['innerArrayItem', [2, 3]], 'Last Item'];
--- arrays.spec.js
+++ arrays.spec.js
@@ -1,9 +1,16 @@
-describe('returnArray',function(){
-   it('should return an array', function(){
-      expect(Array.isArray(returnArray())).to.equal(true);
-   });
-   var arr = returnArray('item1', 'item2');
-   it('should return an array containing the two arguments passed into returnArray', function(){
-      expect(arr[0]).to.equal('item1');
-   });
-})
+describe('returnArray', () => {
+  it('should return an array', () => {
+    expect(Array.isArray(returnArray())).to.equal(true);
+  });
+  var arr = returnArray('item1', 'item2');
+  it('should return an array containing the two arguments passed into returnArray', () => {
+    expect(arr[0]).to.equal('item1');
+  });
+});
+
+describe('returnItemAtArrayIndex', () => {
+  it('should return the item on arr1 at the passed index', () => {
+    expect(returnItemAtArrayIndex(2)).to.equal(3);
+    expect(Array.isArray(returnItemAtArrayIndex(3))).to.equal(true);
+  });
+});
